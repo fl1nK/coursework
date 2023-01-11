e8a09b88c5e6360f70cd3ee96e49bd032b287bd3
= <=>
2023-01-11 20:03:05 +0200

    кінець

--- models/User.js
+++ models/User.js
@@ -1,6 +1,5 @@
 const {Schema, model} = require('mongoose')
 
-
 const User = new Schema({
     username: {type: String, unique: true, required: true},
     password: {type: String, required: true},
